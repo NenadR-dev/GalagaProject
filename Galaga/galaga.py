eea861782db2147ab5ead6ec6b24ec77531cf349
Nenad_Radulovic <popcornsftw2011@hotmail.com>
2018-12-26 16:33:52 +0100

    LocalCommit
    13

--- Galaga/galaga.py
+++ Galaga/galaga.py
@@ -1,41 +1,23 @@
 import sys
 from PyQt5.QtCore import QSize
-from PyQt5.QtGui import QMovie, QPainter
+from PyQt5.QtGui import QMovie, QPainter, QIcon
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from Galaga.Widgets import MainWidget, MultiPlayerWidget
 
+class Menu(QMainWindow):
+    def __init__(self):
+        self.current = MainWidget.MainWindow()
 
-class UIWindow(QWidget):
-    def __init__(self, parent=None):
-        super(UIWindow, self).__init__(parent)
-        self.resize(QSize(800, 600))
-
-
-class MainWindow(QMainWindow):
-    def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent)
-        self.setGeometry(50, 50, 800, 600)
-        self.setFixedSize(800, 600)
-        self.startUIWindow()
-
-        self.movie = QMovie("Galaga/img/space-background.gif")
-        self.movie.frameChanged.connect(self.repaint)
-        self.movie.start()
-
-    def startUIWindow(self):
-        self.Window = UIWindow(self)
-        self.setWindowTitle("PyGalaga")
-        self.show()
-
-    def paintEvent(self, event):
-        currentFrame = self.movie.currentPixmap()
-        frameRect = currentFrame.rect()
-        frameRect.moveCenter(self.rect().center())
-        if frameRect.intersects(event.rect()):
-            painter = QPainter(self)
-            painter.drawPixmap(frameRect.left(), frameRect.top(), currentFrame)
+    def ChangeWidget(self):
+        if(MainWidget.MainWindow.btnsingleplayer.clicked):
+            self.current = MainWidget.MainWindow()
+        elif(MainWidget.MainWindow.btnmultiplayer.clicked):
+            self.current = MultiPlayerWidget.MainWindow()
+        else:
+            self.close()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    w = MainWindow()
+    w = Menu()
     sys.exit(app.exec_())
