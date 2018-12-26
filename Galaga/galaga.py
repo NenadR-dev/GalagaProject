af6c51451d966152fb9b6a2d8b9bed6088733c5c
Stefan Milenković <milenko2204@gmail.com>
2018-12-26 21:25:54 +0100

    Crashing solved

--- Galaga/galaga.py
+++ Galaga/galaga.py
@@ -1,22 +1,7 @@
 import sys
-from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtGui import QMovie, QPainter, QIcon
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
-
-
-from Galaga.gameplay import Gameplay
-import time
-import threading
-
+from PyQt5.QtGui import QMovie, QPainter
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from gameplay import Gameplay
-from multiprocessing import Process
-
-
-
-class UIWindow(QWidget):
-    def __init__(self, parent=None):
-        super(UIWindow, self).__init__(parent)
-        self.resize(QSize(800, 600))
 
 
 class MainWindow(QMainWindow):
@@ -24,25 +9,24 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__(parent)
         self.setGeometry(0, 0, 800, 600)
         self.setFixedSize(800, 600)
-        self.startUIWindow()
+        self.start_ui_window()
 
-        self.movie = QMovie("img/space-background.gif")
+        self.movie = QMovie("Galaga/img/space-background.gif")
         self.movie.frameChanged.connect(self.repaint)
         self.movie.start()
 
-    def startUIWindow(self):
-        #self.Window = UIWindow(self)
+    def start_ui_window(self):
         self.Window = Gameplay(self)
         self.setWindowTitle("PyGalaga")
         self.show()
 
     def paintEvent(self, event):
-        currentFrame = self.movie.currentPixmap()
-        frameRect = currentFrame.rect()
-        frameRect.moveCenter(self.rect().center())
-        if frameRect.intersects(event.rect()):
+        current_frame = self.movie.currentPixmap()
+        frame_rect = current_frame.rect()
+        frame_rect.moveCenter(self.rect().center())
+        if frame_rect.intersects(event.rect()):
             painter = QPainter(self)
-            painter.drawPixmap(frameRect.left(), frameRect.top(), currentFrame)
+            painter.drawPixmap(frame_rect.left(), frame_rect.top(), current_frame)
 
     def keyPressEvent(self, event):
         window = self.Window
--- Galaga/gameplay.py
+++ Galaga/gameplay.py
@@ -1,11 +1,29 @@
-import sys
-from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QGroupBox, QGridLayout, QHBoxLayout, QVBoxLayout, QWidget, QApplication, QLabel
+from PyQt5.QtCore import QSize, Qt, QObject, pyqtSignal, pyqtSlot
+from PyQt5.QtWidgets import QWidget, QLabel
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import QThread, QObject, pyqtSignal, pyqtSlot
+import time, threading
 
-import time
-import threading
+
+def avatars_movement(self, event):
+    avatar1 = self.label_avatar1
+    avatar2 = self.label_avatar2
+    key = event.key()
+
+    if key == Qt.Key_Left:
+        if avatar1.x() > 10:
+            avatar1.move(avatar1.x() - 10, avatar1.y())
+
+    elif key == Qt.Key_Right:
+        if avatar1.x() < 740:
+            avatar1.move(avatar1.x() + 10, avatar1.y())
+
+    elif key == Qt.Key_A:
+        if avatar2.x() > 10:
+            avatar2.move(avatar2.x() - 10, avatar2.y())
+
+    elif key == Qt.Key_D:
+        if avatar2.x() < 740:
+            avatar2.move(avatar2.x() + 10, avatar2.y())
 
 
 class Gameplay(QWidget, QObject):
@@ -14,56 +32,46 @@ class Gameplay(QWidget, QObject):
 
     def __init__(self, parent=None):
         super(Gameplay, self).__init__(parent)
-        thread = threading.Thread(target=self.moveEnemies)
+        thread = threading.Thread(target=self.enemies_movement)
         # make test_loop terminate when the user exits the window
         thread.daemon = True
 
-        self.labelAvatar1 = QLabel(self)
-        self.labelAvatar2 = QLabel(self)
+        self.label_avatar1 = QLabel(self)
+        self.label_avatar2 = QLabel(self)
         self.resize(QSize(800, 600))
         self.list = []
-        self.initUI()
+        self.init_ui()
         thread.start()
 
-    def initUI(self):
-
-        self.labelAvatar = QLabel(self)
-        self.avatar = QPixmap("img/avatar.png")
-        self.avatar = self.avatar.scaled(50, 50)
-        self.labelAvatar.setPixmap(self.avatar)
-        self.labelAvatar.move(10, 540)
-
-
-        avatar1 = QPixmap("img/avatar.png")
+    def init_ui(self):
+        avatar1 = QPixmap("Galaga/img/avatar.png")
         avatar1 = avatar1.scaled(50, 50)
-        self.labelAvatar1.setPixmap(avatar1)
-        self.labelAvatar1.move(10, 540)
+        self.label_avatar1.setPixmap(avatar1)
+        self.label_avatar1.move(10, 540)
 
-        avatar2 = QPixmap("img/avatar.png")
-        avatar2 = avatar1.scaled(50, 50)
-        self.labelAvatar2.setPixmap(avatar2)
-        self.labelAvatar2.move(740, 540)
-
+        avatar2 = QPixmap("Galaga/img/avatar2.png")
+        avatar2 = avatar2.scaled(50, 50)
+        self.label_avatar2.setPixmap(avatar2)
+        self.label_avatar2.move(740, 540)
 
         for i in range(0, 10):
             for j in range(0, 3):
-                self.labelEnemy = QLabel(self)
-                self.enemy = QPixmap("img/enemy.png")
-                self.enemy = self.enemy.scaled(50, 50)
-                self.labelEnemy.setPixmap(self.enemy)
-                self.labelEnemy.move(150 + i*50, 10 + j*50)
-                self.list.append(self.labelEnemy)
+                label_enemy = QLabel(self)
+                enemy = QPixmap("Galaga/img/enemy.png")
+                enemy = enemy.scaled(50, 50)
+                label_enemy.setPixmap(enemy)
+                label_enemy.move(150 + i*50, 10 + j*50)
+                self.list.append(label_enemy)
 
         self.setGeometry(0, 0, 800, 600)
         self.setWindowTitle('PyGalaga')
         self.show()
 
-
-    def startThread(self):
+    def start_thread(self):
         self.thread.start()
 
     @pyqtSlot()
-    def moveEnemies(self):
+    def enemies_movement(self):
         direction = "left"
         while True:
             if direction == "left":
@@ -76,28 +84,9 @@ class Gameplay(QWidget, QObject):
                     self.list[i].move(self.list[i].x() + 10, self.list[i].y())
                 if self.list[29].x() == 740:
                     direction = "left"
-            time.sleep(0.2)
-
+            time.sleep(0.8)
 
     def keyPressEvent(self, event):
-
-        avatar1 = self.labelAvatar1
-        avatar2 = self.labelAvatar2
-        key = event.key()
-
-        if key == Qt.Key_Left:
-            if avatar1.x() > 10:
-                avatar1.move(avatar1.x() - 10, avatar1.y())
-
-        elif key == Qt.Key_Right:
-            if avatar1.x() < 740:
-                avatar1.move(avatar1.x() + 10, avatar1.y())
-
-        if key == Qt.Key_A:
-            if avatar2.x() > 10:
-                avatar2.move(avatar2.x() - 10, avatar2.y())
-
-        elif key == Qt.Key_D:
-            if avatar2.x() < 740:
-                avatar2.move(avatar2.x() + 10, avatar2.y())
-
+        thread = threading.Thread(target=avatars_movement(self, event))
+        thread.isDaemon()
+        thread.start()
