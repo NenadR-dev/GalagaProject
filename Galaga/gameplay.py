0ebf5880fc3df6e23e99fe485b1e3f7f65407c2b
Stefan Milenković <milenko2204@gmail.com>
2018-12-26 16:33:36 +0100

    gameplay ui
    - show avatar and enemies

---
+++ Galaga/gameplay.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt5.QtCore import QSize
+from PyQt5.QtWidgets import QGroupBox, QGridLayout, QHBoxLayout, QVBoxLayout, QWidget, QApplication, QLabel
+from PyQt5.QtGui import QPixmap
+
+
+class Gameplay(QWidget):
+    def __init__(self, parent=None):
+        super(Gameplay, self).__init__(parent)
+        self.resize(QSize(800, 600))
+
+    def initUI(self):
+        labelAvatar = QLabel(self)
+        avatar = QPixmap("img/avatar.png")
+        avatar = avatar.scaled(50, 50)
+        labelAvatar.setPixmap(avatar)
+        labelAvatar.move(10, 540)
+
+        for i in range(0, 10):
+            for j in range(0, 3):
+                labelEnemy = QLabel(self)
+                enemy = QPixmap("img/enemy.png")
+                enemy = enemy.scaled(50, 50)
+                labelEnemy.setPixmap(enemy)
+                labelEnemy.move(150 + i*50, 10 + j*50)
+
+        self.setGeometry(50, 50, 800, 600)
+        self.setWindowTitle('PyGalaga')
+        self.show()
